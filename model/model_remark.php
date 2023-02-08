c9e5478ef0b6918e218094c64d5854faba2b227d
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-02-08 21:21:51 +0900

    feat: 비고 입력 구현

---
+++ model/model_remark.php
@@ -0,0 +1,27 @@
+<?php
+// LOG
+// DB
+include_once(__DIR__ . "/../database/dbconnect.php");
+// POST 
+$memo = $_POST["memo"];
+$remark_category = $_POST["remark_category"];
+$memo = $_POST["memo"];
+$id = $_POST["id"];
+
+// CHANGE SQL BY CATEGORY
+if ($remark_category == "schedule") {
+        $sql = 'UPDATE list_schedule SET schedule_memo = ? WHERE schedule_id = ?;';
+} else {
+        $sql = 'UPDATE list_record SET record_memo = ? WHERE record_id = ?;';
+}
+// UPDATE
+$stmt = $db->prepare($sql);
+$stmt->bind_param("ss", $memo, $id);
+$stmt->execute();
+
+// logInsert($db, $_SESSION['Id'], '', $athlete_name . "-" . $athlete_country . "-" . $athlete_schedule);
+
+echo "<script>
+alert('비고 입력 완료');
+window.location.href='../view/view_schedule.php';
+</script>";
