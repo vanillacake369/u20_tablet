a29acf98a40e8ebec7c11efed10d78d8950b5412
NGN-Limjihoon <lonelynight1026@gmail.com>
2023-03-02 17:31:57 +0900

    fix: 로그인 시 비밀번호 비교 로직 수정

--- auth/signIn.php
+++ auth/signIn.php
@@ -17,10 +17,13 @@ if (!isset($_POST['id']) || $_POST['id'] == "") {
     $_SERVER['REMOTE_ADDR'] == '::1' ? $ip_add = '127.0.0.1' : $ip_add = $_SERVER['REMOTE_ADDR'];
     $currentDate = date('Y-m-d H:i:s');
 
+    // get pw of judge, and compare
     $judgesql = " SELECT * FROM list_judge WHERE judge_account = '" . $id . "';";
     $judgerow = $db->query($judgesql);
-
-    if (($judgedata = mysqli_fetch_array($judgerow)) && (hash('sha256', $pw) == $judgedata['judge_password'])) {
+    $judgedata = mysqli_fetch_array($judgerow);
+    $db_pw = $judgedata['judge_password'];
+    $pw = hash("sha256", $pw);
+    if ($db_pw == $pw) {
         $sql = "UPDATE list_judge SET judge_latest_datetime = ?, judge_latest_ip= ?, judge_latest_session = ? WHERE judge_account = ?";
         $stmt = $db->prepare($sql);
         $stmt->bind_param("ssss", $currentDate, $ip_add, $session, $id);
